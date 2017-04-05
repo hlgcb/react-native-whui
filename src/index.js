2f7c15d8bb1ab01be662beca2a37c4c5479dcd42
LiNing <lining@91hong.com.cn>
2017-04-05 18:26:49 +0800

    添加TabBar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import Button from './buttons/Button';
 import BaseView from './views/BaseView';
-
+import TabBar from './views/TabBar'
 const Elements = {
 	BaseView,
-	Button
+	Button,
+	TabBar,
 };
 
 module.exports = Elements;
