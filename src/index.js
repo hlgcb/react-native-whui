4246f151f15e3e224e2add01cd8bacdb8f7b99b7
LiNing <lining@91hong.com.cn>
2017-04-06 17:34:22 +0800

    添加导出

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import Button from './buttons/Button';
 import BaseView from './views/BaseView';
 import TabBar from './TabBar/Tabbar';
 import TextInput from './forms/TextInput';
-
+import Card from './Card/Card.js'
 const Elements = {
 	BaseView,
 	Button,
 	TabBar,
-	TextInput
+	TextInput,
+	Card
 };
 
 module.exports = Elements;
