b240ad0175fa2dc09a44fad5a22f579ea2fb415c
Li Ming <liming@91hong.com.cn>
2017-04-06 10:54:05 +0800

    按钮优化

--- src/buttons/Button.js
+++ src/buttons/Button.js
@@ -1,14 +1,42 @@
 /**
  * 按钮
+ import { Button } from 'react-native-whui';
+ <Button
+ 	title="按钮文案"
+	icon={可选参数，图标，暂未支持}
+	loading={true，是不是加载中，加载中不会触发 onPress 事件}
+	loadingText="加载中的文案，如果没文案，使用默认文案"
+	disabled={true，按钮不可用}
+	onPress={按钮点击事件}
+	backgroundColor="背景色"
+	color="文字颜色"
+	highlightBackgroundColor="高亮背景色"
+	disabledBackgroundColor="禁用状态背景色"
+	disabledColor="禁用状态文字颜色"
+	 />
  */
 import React, { Component, PropTypes } from 'react';
 import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
 import Theme from './../theme/default.js';
 
 class Button extends Component {
+
+	constructor(props){
+		super(props);
+		this.pressHandler = this.pressHandler.bind(this);
+	}
+
+	pressHandler(){
+		// 按钮处于加载中和禁用状态，都不响应事件
+		if((this.props.loading !== true || this.props.disabled !== true)
+				&& typeof this.props.onPress == "function"){
+			this.props.onPress();
+		}
+	}
+
 	render(){
 		return (
-			<TouchableOpacity onPress={this.props.onPress} activeOpacity={0.5}>
+			<TouchableOpacity onPress={this.pressHandler} activeOpacity={0.5}>
 				<View style={styles.button}>
 					<Text style={styles.buttonText}>
 					{this.props.title}
@@ -22,6 +50,11 @@ class Button extends Component {
 // Props 类型检查
 Button.propTypes = {
 	title: PropTypes.string.isRequired,
+	icon: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+	loading: PropTypes.bool,
+	loadingText: PropTypes.string,
+	disabled: PropTypes.bool,
+	onPress: PropTypes.func,
 	// 取值参考：https://facebook.github.io/react/docs/typechecking-with-proptypes.html
 	// PropTypes.any,
 	// PropTypes.object,
@@ -32,7 +65,12 @@ Button.propTypes = {
 }
 // 默认 props 值
 Button.defaultProps = {
-	title: "确定"
+	title: "确定",
+	icon: null,
+	loading: false,
+	loadingText: "处理中，请稍候",
+	disabled: false,
+	onPress: ()=>{}
 };
 
 const styles = StyleSheet.create({
