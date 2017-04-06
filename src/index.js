646af7fa4550329e6defbfeca32f007fdaa0cd51
Li Ming <liming@91hong.com.cn>
2017-04-06 09:48:10 +0800

    修正 Tabbar 的路径

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Button from './buttons/Button';
 import BaseView from './views/BaseView';
-import TabBar from './views/TabBar'
+import TabBar from './Tabbar/TabBar';
+
 const Elements = {
 	BaseView,
 	Button,
