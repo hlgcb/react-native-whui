67354e2bae2aaebb8e0ffb6d7f42ef86ca8ecec5
LiNing <lining@91hong.com.cn>
2017-04-07 15:55:08 +0800

    适配card

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -46,9 +46,9 @@ export default class Card extends Component{
 
 const styles = StyleSheet.create({
   cardStyle : {
-      marginVertical: 5,
+      marginVertical: 8,
       marginHorizontal: 2,
-      flex: 1,
+
       borderWidth: (1/PixelRatio.getPixelSizeForLayoutSize(1)),
       borderRadius: 2,
       borderColor: Theme.color.card_borderColor,
