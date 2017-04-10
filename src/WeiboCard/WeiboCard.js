0c903cd9df135f21a356910d2f33362e09fa402c
duanbingyang <duanby0426@sina.com>
2017-04-10 14:12:34 +0800

    微博卡片修改

--- src/WeiboCard/WeiboCard.js
+++ src/WeiboCard/WeiboCard.js
@@ -2,7 +2,16 @@
  * 微博单条卡片
  import { WeiboCard } from 'react-native-whui';
  <WeiboCard
- 	
+ 	text:'',		//微博文案
+	time:'',		//微博时间
+	timeIcon:'',	//时间图标
+	imgUrl: [],		//微博图片
+	btnTitle: "",	//按钮标题
+	btnIcon: null,	//按钮图标
+	btnLoading: false,
+	btnLoadingText: "处理中，请稍候",
+	btnDisabled: false,
+	btnOnPress: () => { }
 	 />
  */
 import React, { Component, PropTypes } from 'react';
@@ -26,27 +35,34 @@ class WeiboCard extends Component {
 		}
 	}
 
+	imgBox(arr){
+		let imgArr = [];
+		for(let i = 0;i<arr.length;i++){
+			imgArr.push(<Image key={i} resizeMode="contain" style={styles.image} source={{uri:arr[i]}} />)
+		}
+		console.log(imgArr)
+		return imgArr;
+	}
+
 	render() {
 		return (
 			<View>
 				<Card>
 					<View style={styles.timeBox}>
-						<Image resizeMode="contain" style={styles.timeIcon} source={{uri:'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=300268425,1269595726&fm=58'}} />
-						<Text>03-28 10:20</Text>
+						<Image resizeMode="contain" style={styles.timeIcon} source={require('../../../../app/images/weiboCard/timeIcon.png')} />
+						<Text style={styles.timeText}>{this.props.time}</Text>
 					</View>
-					<View>
-						<Text>#国庆七天旅程#妹子好美的地方啊，亲们，快来看看这里是哪里吧，我家后院的草原～～</Text>
+					<View style={styles.detial}>
+						<Text style={styles.detialText}>{this.props.text}</Text>
 						<View style={styles.imageBox}>
-							<Image resizeMode="contain" style={styles.image} source={{uri:'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=300268425,1269595726&fm=58'}} />
-							<Image resizeMode="contain" style={styles.image} source={{uri:'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=300268425,1269595726&fm=58'}} />
-							<Image resizeMode="contain" style={styles.image} source={{uri:'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=300268425,1269595726&fm=58'}} />
+							{this.imgBox(this.props.imgUrl)}
 						</View>
 					</View>
 					<View>
 						<Button
-							title="立即投放"
-							backgroundColor="#FFF"
-							color="#508CEE"
+							title={this.props.btnTitle}
+							backgroundColor={this.props.btnBackgroundColor}
+							color={this.props.btnColor}
 						 />
 					</View>
 				</Card>
@@ -57,12 +73,11 @@ class WeiboCard extends Component {
 
 // Props 类型检查
 WeiboCard.propTypes = {
-	title: PropTypes.string.isRequired,
+	title: PropTypes.string,
 	icon: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
 	loading: PropTypes.bool,
 	loadingText: PropTypes.string,
 	disabled: PropTypes.bool,
-	fixPosition: PropTypes.oneOf(['bottom']),
 	onPress: PropTypes.func,
 	// 取值参考：https://facebook.github.io/react/docs/typechecking-with-proptypes.html
 	// PropTypes.any,
@@ -74,27 +89,54 @@ WeiboCard.propTypes = {
 }
 // 默认 props 值
 WeiboCard.defaultProps = {
-	title: "确定",
-	icon: null,
-	loading: false,
-	loadingText: "处理中，请稍候",
-	disabled: false,
-	fixPosition: null,
-	onPress: () => { }
+	text:'#国庆七天旅程#妹子好美的地方啊，亲们，快来看看这里是哪里吧，我家后院的草原～～',
+	time:'03-26 10:20',
+	timeIcon:'../../../../app/images/weiboCard/timeIcon.png',
+	imgUrl: ['https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=300268425,1269595726&fm=58','https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=300268425,1269595726&fm=58','https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=300268425,1269595726&fm=58'],
+	btnTitle: "立即投放",
+	btnIcon: null,
+	btnLoading: false,
+	btnColor: '#508CEE',
+	btnBackgroundColor: '#FFF',
+	btnLoadingText: "处理中，请稍候",
+	btnDisabled: false,
+	btnOnPress: () => { }
 };
 
 const styles = StyleSheet.create({
 	timeBox: {
 		backgroundColor: '#FFF',
+		flexDirection: 'row',
+		justifyContent: 'flex-start',
+		marginLeft: Theme.size(28),
+		paddingTop: Theme.size(30),
+		paddingBottom: Theme.size(30),
+
 	},
 	timeIcon:{
-		width: 30,
-		height:30,
+		width: Theme.size(30),
+		height: Theme.size(30),
+		marginRight: Theme.size(24),
+		marginTop:Theme.size(2)
+	},
+	timeText: {
+		color: '#666',
+		fontSize: Theme.size(30),
+	},
+	detial: {
+		backgroundColor: '#F7F7F7',
+		paddingTop:Theme.size(30),
+	},
+	detialText: {
+		marginLeft: Theme.size(26),
+		marginRight: Theme.size(26),
+		color: '#151515',
+		fontSize: Theme.size(32),
+
 	},
 	imageBox: {
 		flexDirection: 'row',
 		justifyContent: 'flex-start',
-		backgroundColor: '#C00',
 		paddingLeft: Theme.size(16),
 		marginBottom: Theme.size(30),
 		marginTop: Theme.size(20),
